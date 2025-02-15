615dde6e6e6aaf72b4e6445242c364b623b6c03d
Shivam dell <shivamsharma199802@gmail.com>
2025-02-15 23:20:46 +0530

    custom user model

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,86 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = [
+        "pkid",
+        "id",
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_staff",
+        "is_active",
+    ]
+    list_display_links = ["id", "email"]
+    list_filter = [
+        "is_staff",
+        "is_active",
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+    ]
+    fieldsets = (
+        (
+            _("Login Credentials"),
+            {
+                "fields": (
+                    "email",
+                    "password",
+                ),
+            },
+        ),
+        (
+            _("Personal Information"),
+            {
+                "fields": (
+                    "username",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            _("Permissions and Groups"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            _("Important Dates"),
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+
+    search_fields = ["username", "first_name", "last_name", "email"]
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    username = models.CharField(max_length=150, verbose_name=_("Username"), unique=True)
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name=_("First Name"),
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name=_("Last Name"),
+    )
+    email = models.EmailField(
+        verbose_name=_("Email Address"),
+        unique=True,
+    )
+    is_staff = models.BooleanField(
+        default=False,
+        verbose_name=_("Staff Status"),
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_("Active"),
+    )
+    date_joined = models.DateTimeField(
+        verbose_name=_("Date Joined"),
+        default=timezone.now,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name.title(), self.last_name.title()}"
+
+    def get_sort_name(self):
+        return self.username
